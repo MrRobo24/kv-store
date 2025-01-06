75b43f6300dea65553552c928e736c22cf72b61f
MrRobo24 <sumanarpit.24@gmail.com>
2025-01-07 01:03:10 +0530

    setup single thread for writing log

--- kvstore.py
+++ kvstore.py
@@ -1,15 +1,33 @@
+from exception import CustomException
+import threading
+import queue
+
 class KVStore:
+  _kv_store = None
+  
   def __init__(self, log_file="kv_store.log"):
-    self.kv_store = {}
+    if (KVStore._kv_store != None):
+      raise(CustomException("KVStore is a singleton class"))
+    
+    KVStore._kv_store = {}
     self.log_file = log_file
+    self.log_queue = queue.Queue()
+    self.writer_thread = threading.Thread(target=self._write_to_log, daemon=True)
+    self.writer_thread.start()
     self._load_from_log()
     
-  def _log_change(self, command, key, value=None):
-    with open(self.log_file, "a") as f:
-      if command == "PUT":
-        f.write(f"PUT {key} {value}\n")
-      elif command == "REMOVE":
-        f.write(f"REMOVE {key}\n")
+  def _log_change(self, command, key, value = None):
+    self.log_queue.put((command, key, value))
+    
+  def _write_to_log(self):
+    while True:
+      command, key, value = self.log_queue.get()
+      with open(self.log_file, "a") as f:
+        if command == "PUT":
+          f.write(f"PUT {key} {value}\n")
+        elif command == "REMOVE":
+          f.write(f"REMOVE {key}\n")
+      self.log_queue.task_done()
         
   def _load_from_log(self):
     try:
@@ -18,28 +36,28 @@ class KVStore:
           parts = line.strip().split()
           if parts[0] == "PUT" and len(parts) == 3:
             key, value = parts[1], parts[2]
-            self.kv_store[key] = value
+            KVStore._kv_store[key] = value
           elif parts[0] == "REMOVE" and len(parts) == 2:
             key = parts[1]
-            if key in self.kv_store:
-              del self.kv_store[key]
+            if key in KVStore._kv_store:
+              del KVStore._kv_store[key]
     except FileNotFoundError:
       print('No previous log file found')
     
   def put(self, key, val):
     if not key.strip() == '':
       self._log_change("PUT", key, val)
-      self.kv_store[key] = val
+      KVStore._kv_store[key] = val
       return val
     return None
     
   def get(self, key):
-    return self.kv_store.get(key, None)
+    return KVStore._kv_store.get(key, None)
   
   def remove(self, key):
-    if self.kv_store.get(key, None):
+    if KVStore._kv_store.get(key, None):
       self._log_change("REMOVE", key)
-      del self.kv_store[key]
+      del KVStore._kv_store[key]
       return True
     else:
       return False
--- kvstoreserver.py
+++ kvstoreserver.py
@@ -1,9 +1,15 @@
+from exception import CustomException
 from kvstore import KVStore
 import socket
 
 class KVStoreServer:
+  _kv_store = None
+  
   def __init__(self, host='127.0.0.1', port=6369):
-    self.kv_store = KVStore()
+    if (KVStoreServer._kv_store != None):
+      raise(CustomException("KVStoreServer is a singleton class"))
+    
+    KVStoreServer._kv_store = KVStore()
     self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.server_socket.bind((host, port))
     self.server_socket.listen(5)
@@ -22,20 +28,20 @@ class KVStoreServer:
       val = data[2]
     
     if command == 'PUT':
-      resp = self.kv_store.put(key, val)
+      resp = KVStoreServer._kv_store.put(key, val)
       if resp == val:
         print(resp)
         client_socket.sendall(b'OK')
       else: 
         client_socket.sendall(b'KV_ERROR')
     elif command == 'GET':
-      val = self.kv_store.get(key)
+      val = KVStoreServer._kv_store.get(key)
       if val:
         client_socket.sendall(val.encode())
       else:
         client_socket.sendall(b'NOT_FOUND')
     elif command == 'REMOVE':
-      if self.kv_store.remove(key):
+      if KVStoreServer._kv_store.remove(key):
         client_socket.sendall(b'OK')
       else:
         client_socket.sendall(b'NOT_FOUND')
